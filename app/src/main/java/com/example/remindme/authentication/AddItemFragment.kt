07fee131a0dce63f372bd4bd6e0f5edb7c8f0d0c
Srijan Shrestha <srijans318@gmail.com>
2023-04-07 21:34:50 -0400

    Work on Add item design

---
+++ app/src/main/java/com/example/remindme/authentication/AddItemFragment.kt
@@ -0,0 +1,20 @@
+package com.example.remindme.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.remindme.R
+
+
+class AddItemFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_item, container, false)
+    }
+}
